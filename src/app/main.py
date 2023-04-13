0552d6d981d7fe94ca65fbfa1d325b6623a1890c
Messias Nunesmaia <messias.lsn@gmail.com>
2023-04-13 17:49:54 -0300

    Liberacao de CORS

--- src/app/main.py
+++ src/app/main.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 # TODO utilizar AWS para armazenar a chave secreta e parar de gerar aleatória
 secret_key = Fernet.generate_key()
 
+@app.after_request
+def set_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+    return response
+
 @app.route('/generate_url', methods=['POST'])
 def generate_url():
     return GenerateUrlController(secret_key).generate_url()
