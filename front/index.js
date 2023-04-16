10817a48771fac78f9bb40c26c19e339f3710272
Messias Nunesmaia <messias.lsn@gmail.com>
2023-04-15 22:09:57 -0300

    Regras de negocio para criacao de senha automatica

--- front/index.js
+++ front/index.js
@@ -1,19 +1,33 @@
+const gateway_url = "http://127.0.0.1:5000/";
+const s3_url = "http://localhost:4566/static-bucket/";
+
 const code = Math.floor(Math.random() * 90000) + 10000;
 const spancode = document.getElementById("code");
 spancode.innerHTML = code;
+
+const passwordOptions = document.querySelector('#passwordOptions');
+const passwordInput = document.querySelector('#password');
+
+passwordOptions.addEventListener('change', () => {
+    if (passwordOptions.value === 'custom') {
+        passwordInput.removeAttribute('disabled');
+    } else {
+        passwordInput.value = ''
+        passwordInput.setAttribute('disabled', '');
+    }
+});
 
 const form = document.getElementById("myForm");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const formData = new FormData(form);
     formData.append("code", code);
-    fetch("http://127.0.0.1:5000/generate_url", {
+    fetch(gateway_url + "generate_url", {
         method: "POST",
         body: formData,
-    })
-        .then((response) => response.text())
-        .then((data) => {
-            const link = "http://localhost:4566/static-bucket/show-password.html?uuid=" + data;
+    }).then((response) => response.text())
+    .then((data) => {
+            const link = s3_url + "show-password.html?uuid=" + data;
             document.getElementById("linkSpan").innerHTML = link;
             document.getElementById("link").href = link;
         });
