aefa08e612f12924db3a4d29c25004624037d5b9
Sandro <sandrouh@hotmail.com>
2023-04-16 17:45:20 -0300

    feat: ajustes na interface

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,41 +3,56 @@ import { useState } from "react";
 
 export default function Home() {
   const [result, setResult] = useState('')
+  const [useLetters, setUseLetters] = useState(false)
+  const [useNumbers, setUseNumbers] = useState(false)
+  const [usePunctuations, setUsePunctuations] = useState(false)
+  const [viewCount, setViewCount] = useState('')
+  const [expirationDateInSeconds, setExpirationDateInSeconds] = useState('')
+  const [sendedPassword, setSendedPassword] = useState('')
+
+  const inputStyle = "shadow appearance-none border rounded py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+  const inputErrorStyle = "shadow appearance-none border rounded border-red-500 py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
 
   return (
-    <main className="flex min-h-screen flex-col  p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1 className='text-lg'>Totvs Test</h1>
+    <main className="flex min-h-screen flex-col font-mono p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between  text-sm lg:flex">
+        <h1 className='text-2xl'>Totvs Test</h1>
       </div>
       <div className="p-24" >
-        <p className="text-center text-2xl">Utilize este app para gerar uma nova senha</p>
+        <p className="text-2xl ">Utilize este app para gerar uma nova senha</p>
         <div className="flex flex-col items-start justify-between w-80 pt-12">
           <p className="pb-6">Quais caracteres sua senha pode conter?</p>
-          <div className="flex justify-between w-3/6">
+          <div className="flex justify-between w-2/6">
             <label>Alfabeto</label>
-            <input type="checkbox" value='' />
+            <input type="checkbox" onChange={(event) => setUseLetters(event.target.checked)} />
           </div>
-          <div className="flex justify-between w-3/6">
+          <div className="flex justify-between w-2/6">
             <label>Números</label>
-            <input type="checkbox" value='' />
+            <input type="checkbox" onChange={(event) => setUseNumbers(event.target.checked)} />
           </div>
-          <div className="flex justify-between w-3/6">
+          <div className="flex justify-between w-2/6">
             <label>Pontuação</label>
-            <input type="checkbox" value='' />
+            <input type="checkbox" onChange={(event) => setUsePunctuations(event.target.checked)} />
           </div>
         </div>
 
         <p className="pt-8 pb-4">Quantas vezas a senha poderá ser vista após ser salva?</p>
-        <input type="number" className="w-12 border-cyan-950 border-1 rounded-sm"></input>
+        <input type="number" className={`${inputStyle} w-18`}></input>
+        <div>
+
+        </div>
+        <p className="pt-8 pb-4">Por quanto tempo a senha ficará disponível ( em segundos )?</p>
+
+        <input type="number" className={`${inputStyle} w-18`}></input>
 
         <p className="pt-4">Se você desejar digitar a sua própria senha, utilize o campo abaixo, senão, deixe-o em branco</p>
-        <input type="text" className="w-64 mt-4 border-cyan-950 border-1 rounded-sm"></input>
-        <div>
-          <button className="bg-green-500 text-white rounded-md mt-4 p-2">Quero gerar a senha no backend</button>
-          <button className="bg-slate-300 rounded-md ml-4 p-2">Quero ver a senha agora mesmo</button>
+        <input type="text" className={`${inputStyle} w-80 mt-4`}></input>
+        <div className="flex flex-wrap gap-2 mt-4">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Gerar a senha no backend</button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Ver a senha agora mesmo</button>
         </div>
-        <div>
-          <button className="bg-slate-300 rounded-md mt-4 p-2 " onClick={() => setResult(generateRandomPassword(20, true, true, true))}>Salvar</button>
+        <div className="mt-4">
+          <button className="bg-green-700 hover:bg-green-500 text-white font-bold py-2 px-4 rounded" onClick={() => setResult(generateRandomPassword(20, true, true, true))}>Salvar</button>
         </div>
         <p className="m-4" >{result}</p>
       </div>
