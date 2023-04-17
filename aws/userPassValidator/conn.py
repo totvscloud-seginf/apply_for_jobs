3a4d1584d259be7e2d49b60775133898686191f1
Philipe Couto <philipe.couto@zkteco.com>
2023-04-17 09:09:08 -0300

    novas funcões de backend adicionadas

--- aws/userPassValidator/conn.py
+++ aws/userPassValidator/conn.py
@@ -18,9 +18,9 @@ class Database:
         try:
             user_id = int(uuid.uuid4())
             user['userid'] = user_id
-            self.table.put_item(Item=user)
+            self.userTable.put_item(Item=user)
             
-            passlifetime = self.calculate_passlifetime(userpass_item['passlifetime'])
+            passlifetime = self.calculate_passlifetime(password['passlifetime'])
             
             userpass_item = {
                 'userid': user_id,
@@ -32,20 +32,37 @@ class Database:
             }
 
             try:
-                self.table.put_item(Item=userpass_item, ConditionExpression='attribute_not_exists(userid) and attribute_not_exists(passid)')
+                self.passTable.put_item(Item=userpass_item, ConditionExpression='attribute_not_exists(userid) and attribute_not_exists(passid)')
                 return {'result': 'OK', 'err_code': 0, 'data': 'Dados registrados com sucesso'}
             except ClientError as e:
                 error_code = e.response['Error']['Code']
                 if error_code == 'ConditionalCheckFailedException':
                     return {'result': 'ERRO', 'err_code': 1, 'data': 'Já existe um usuário com esse userid ou passid'}
                 else:
-                    return {'result': 'ERRO', 'err_code': 1, 'data': f"Erro ao adicionar item: {str(e)}"}
+                    return {'result': 'ERRO', 'err_code': 1, 'data': f"Erro ao adicionar usuário: {str(e)}"}
 
         except Exception as e:
-            return {'result': 'ERRO', 'err_code': 1, 'data': f"Erro ao adicionar item: {str(e)}"}
+            return {'result': 'ERRO', 'err_code': 1, 'data': f"Erro ao adicionar usuário: {str(e)}"}
     
-            
-    def update_userpass(self, user_id, pass_id, passlimitview):
+    def add_new_pass(self, user_id, newpass):
+        
+        passlifetime = self.calculate_passlifetime(newpass['passlifetime'])
+        userpass_item = {
+                'userid': user_id,
+                'passid': int(uuid.uuid4()),
+                'passlifetime': passlifetime,
+                'passlimitview': newpass.passlimitview,
+                'currentlink': newpass.linkdata,
+                'status': 1,
+            }
+        
+        try:
+            self.passTable.put_item(Item=userpass_item, ConditionExpression='attribute_not_exists(userid) and attribute_not_exists(passid)')
+            return {'result': 'OK', 'err_code': 0, 'data': 'Novo password adicionado'}
+        except Exception as e:
+              return {'result': 'ERRO', 'err_code': 1, 'data': f"Erro ao adicionar password: {str(e)}"}
+
+    def update_userpass_limitview(self, user_id, pass_id, passlimitview):
         """Esse método serve para atualizar na medida em que o usuário clica no link"""
         response = self.passTable.update_item(
             Key={
@@ -57,7 +74,9 @@ class Database:
                 ':v': passlimitview
             }
         )
-        
+
+        return {'result': 'OK', 'err_code': 0, 'data': "limite atualizado com sucesso"}
+    
     def user_query(self, login):
         """ Metodo para realizar a consulta dos usuários e dos passwords
             Melhorar esse método para permitir salvar um usuário caso ele não exista na base
@@ -72,7 +91,7 @@ class Database:
         user_items = user_response['Items']
          
         if len(user_items) == 0:
-            return {'result': 'WARNING', 'err_code': '0', 'data': 'Login não encontrato'}
+            return {'result': 'WARNING', 'err_code': '1000', 'data': 'Login não encontrato'}
         
         userpass_response = self.passTable.query(
             IndexName='userid-index',
@@ -81,11 +100,10 @@ class Database:
                 ':uid': user_items.userid,
             }
         )
+
         userpass_items = userpass_response['Items']
         
         if len(userpass_items) == 0:
             return {'result': 'WARNING', 'err_code': '0', 'data': 'O login foi identificado mas há um erro no cadastro'}
         
-        user_items = user_response['Items']
-        
-        return  user_items
+        return  userpass_items
--- aws/userPassValidator/user.py
+++ aws/userPassValidator/user.py
@@ -1,31 +1,61 @@
 import json
 import conn
 import base64
-from datatime import datatime, timedelta
+import datetime
 from re import search
 
 class user:
     def __init__(self, userAUTH):
         self.user_login = userAUTH.login
         self.password = userAUTH.password
+    
+    def do_user_login(self, link=None):
+        """Realizar login"""
+        conector = conn()
+
+        if link:
+            link = self.do_user_link_gen()
         
-    def doUserLogin(self):
-        return
-      
+        userlogin = conector.user_query(self.user_login)
+        return userlogin
+     
+    def do_save_new_user(self, newUSERDATA):
+        """Salva novo usuário"""
+        conector = conn()  
+        return  conector.put_item(self.user_login, self.password)
         
-    def doUserLink(self):
+    def do_user_link_gen(self):
+        """Gera o hash do link"""
+        epoch_now = int(datetime.datetime.now().timestamp())
+        encode_time = base64.b64encode(epoch_now.encode("utf-8"))
         userlogin = self.user_login
-        encoded_bytes = base64.b64encode(userlogin.encode("utf-8"))
-        userencrypt = str(encoded_bytes, "utf-8")
+        encoded_login = base64.b64encode(userlogin.encode("utf-8"))
+        userencrypt = {'datetime':  str(encode_time, "utf-8"), 'user':  str(encoded_login, "utf-8")}
        
         return userencrypt
         
-    def doUserPasswordView(self):
+    def do_user_password_view(self, link):
+        """Aqui define se o usuário pode ou não logar com o link de acesso"""
+        conector = conn()  
+        passcountview = conector.user_query(self.user_login)
+        epoch_now = int(datetime.datetime.now().timestamp())
+        if epoch_now > passcountview.passlifetime:
+            return {'result': 'WARNING', 'err_code': '0', 'data': 'O link não é mais válido'}
         
-        return
+        if link != passcountview.currentlink:
+              return {'result': 'WARNING', 'err_code': '0', 'data': 'O link não é mais válido'}
         
-    def doUserPassUpdate(self, userDATA):
-        self.user_login = userDATA.login
-        self.password = userDATA.password
+        limitview = int(passcountview.passlimitview)
+
+        if limitview == 0:                
+            return {'result': 'WARNING', 'err_code': '0', 'data': 'O limite de visualização foi atingido'}
+        
+        conector.update_userpass_limitview(passcountview.userid, passcountview.passid, limitview-1)
+        
+    def do_user_add_newpass(self):
+        conector = conn() 
+        userlogin = conector.user_query(self.user_login)
+        conector.add_new_pass(userlogin.userid, )
+
         
     
